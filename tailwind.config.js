4fd875bb3a5d6661cb61855344f00f66f163eb00
rishabhdas1006 <prin100603@gmail.com>
2025-05-19 10:45:27 +0530

    added initial files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./app/**/*.{js,jsx,ts,tsx}"],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#030014",
+				accent: "#AB8BFF",
+			},
+		},
+	},
+	plugins: [],
+};
