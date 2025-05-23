7dcff509d64ab1ca08bd4f58ecec796675dcf82b
rishabhdas1006 <prin100603@gmail.com>
2025-05-23 12:28:18 +0530

    added homepage features

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,26 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+export const useFetch = <T>(
+	fetchFunction: () => Promise<T>,
+	autoFetch = true
+) => {
 	const [data, setData] = useState<T | null>(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState<Error | null>(null);
 
-    const fetchData = async () => {
-        try {
+	const fetchData = async () => {
+		try {
+			setLoading(true);
+			setError(null);
 
-        }
-        catch(err) {
+			const result = await fetchFunction();
 
-        }
-        finally {
-            setLoading(false);
-        }
-    }
+			setData(result);
+		} catch (err) {
+			setError(
+				err instanceof Error ? err : new Error("An error occured")
+			);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-    const reset = () => {
-        setData(null);
-        setLoading(false);
-        setError(null);
-    }
+	const reset = () => {
+		setData(null);
+		setLoading(false);
+		setError(null);
+	};
+
+	useEffect(() => {
+		if (autoFetch) fetchData();
+	}, []);
+
+	return { data, loading, error, refetch: fetchData, reset };
 };
-
